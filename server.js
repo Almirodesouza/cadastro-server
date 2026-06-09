22cf052ae1461e48d6fb9f4b644972257c4ecddf
Almiro <0almiro0de0souza0@gmail.com>
2026-06-09 13:27:39 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { PrismaClient } from "./generated/prisma/client.js";
+
+const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.get("/usuarios", async (req, res) => {
+    const user = await prisma.user.findMany();
+    res.status(200).json(user);
+});
+
+app.post("/usuarios", async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: Number(req.body.age)
+        }
+    });
+    res.status(201).json(user);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: Number(req.body.age)
+        }
+    });
+    res.status(200).json(user);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json({ message: "Usuário deletado com sucesso" });
+});
+
+app.listen(3000);
